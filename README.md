75a0cfd23bfe9cbb27e83c9c7ac66bce51e78b6a
Philippe MICHEL <philippe@docteur-michel.fr>
2021-06-28 16:04:05 +0200

    Initialisation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# CoPreDex
-Étude CoPreDeX
+# Étude CoPreDeX
+
+
+Comparaison de la Prednisolone et de la Dexaméthasone sur la mortalité à J28 chez des patients sous oxygénothérapie, atteints de la CoViD-19 : étude multicentrique, randomisée, de non-infériorité en ouvert
+
+Ce dépôt ne contient que les scripts R utilisés mais pas les données ou les résultats qui ne m'appartiennent pas.  
---
+++ scripts/.R
@@ -0,0 +1,37 @@
+
+
+#  ------------------------------------------------------------------------
+#
+# Title : tests
+#    By : Philippe MICHEL
+#  Date : 2021-06-28
+#    
+#  ------------------------------------------------------------------------
+
+
+
+# Packages ----------------------------------------------------------------
+
+library("baseph")
+library("factoextra")
+library("FactoMineR")
+library("ggplot2")
+library("ggplotlyExtra")
+library("ggthemes")
+library("kableExtra")
+library("knitr")
+library("lubridate")
+library("magrittr")
+library("markdown")
+library("missMDA")
+library("prettydoc")
+library("purrr")
+library("readr")
+library("rlang")
+library("rmarkdown")
+library("stringr")
+library("survival")
+library("survminer")
+library("table1")
+library("tidyr")
+library("tidyverse")
